94619adb3b5ba70ea552637b491c2e510f11eb12
Sum1likegod <it2037@global.org.in>
2024-07-02 22:38:56 +0530

    learning os Module

---
+++ Clearing_the_clutter.py
@@ -0,0 +1,21 @@
+import os as o
+
+
+o.chdir("New folder/")
+files_in_the_folder = o.listdir()
+
+
+def clear_the_clutter(file_type):
+    file_cat = []
+    for i in range(len(files_in_the_folder)):
+        if files_in_the_folder[i].split(".")[1] == file_type:
+            file_cat.append(files_in_the_folder[i])
+            if i == (len(files_in_the_folder)-1):
+                for j in range(len(file_cat)):
+                    o.rename(file_cat[j], f"{j+1}.{file_type}")
+
+    return 'everything is done perfectly'
+
+
+file_types = input("Enter the file type you want to clear the clutter:")
+print(clear_the_clutter(file_types))
